c952e5621fbb896aafce97f41c6cb83e3a362bd3
rain <yudu233@gmail.com>
2021-10-13 23:39:36 +0800

    add 添加文章&Demo源码 《vue-router原理分析与实践》

---
+++ vue/vue_router/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '../components/HelloWorld.vue'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
